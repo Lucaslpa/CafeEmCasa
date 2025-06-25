665c55b735e5dfa194f2df3d83e0165cd34bf349
lucas <lucaslpa12345@gmail.com>
2025-06-25 17:58:13 -0300

    INICIANDO APP

---
+++ app/src/main/java/com/example/cafeemcasa/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.cafeemcasa.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Red = Color(0xFFE82933)
+val White = Color(0xFFFFFFFF)
+val White10 = Color(0xFFF5F0F0)
+val Black = Color(0xFF171212)
+val LightBrown = Color(0xFF876363)
---
+++ app/src/main/java/com/example/cafeemcasa/ui/theme/Type.kt
@@ -0,0 +1,56 @@
+package com.example.cafeemcasa.ui.theme
+
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.cafeemcasa.R
+import androidx.compose.ui.text.font.Font
+
+
+val PlusJakartaSans = FontFamily(
+    Font(R.font.plusjakartasans_bold, FontWeight.Bold),
+    Font(R.font.plusjakartasans_regular, FontWeight.Normal),
+    Font(R.font.plusjakartasans_medium, FontWeight.Medium),
+ )
+
+val Typography = Typography(
+    titleLarge = TextStyle(
+        fontFamily = PlusJakartaSans,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = PlusJakartaSans,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = PlusJakartaSans,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        lineHeight = 21.sp,
+        letterSpacing = 0.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = PlusJakartaSans,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,),
+    bodyMedium = TextStyle(
+        fontFamily = PlusJakartaSans,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 21.sp,),
+    labelSmall = TextStyle(
+        fontFamily = PlusJakartaSans,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        lineHeight = 18.sp,)
+)
